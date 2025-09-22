94361c01777ecc89d4230b8b5cf0b894eb4e62f4
Shawn <iamshawnhartline@gmail.com>
2025-09-22 11:01:12 -0500

    Deployment

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject, catchError, of, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,10 @@ import { environment } from '../../environments/environment';
 export class UserService {
   currentUserSubject = new BehaviorSubject<User | null>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {}
 
   setCurrentUser(user: User | null){
     this.currentUserSubject.next(user);
@@ -22,7 +26,11 @@ export class UserService {
         this.setCurrentUser(data)
         console.log(this.currentUserSubject);
         console.log(data);  
-      })
+      }),
+      catchError(() => {
+                this.authService.logout();
+                return of(null);
+              })
     )
   }
 }
