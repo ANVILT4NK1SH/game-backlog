ebf1d047a6fb3d1bbbc8cb2aed6f7d5bfd944a95
Shawn <iamshawnhartline@gmail.com>
2025-10-29 05:52:06 -0500

    Styling fixes and tweaks

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -12,9 +12,13 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
+  }
+
+  get isLoginPage(): boolean {
+    return this.router.url === '/login'
   }
 }
